9517ef0f87f037a3117e6a422dbc51814cfd502a
Atila jr <atilla.junior33@gmail.com>
2023-01-17 17:46:52 -0300

    Project created

---
+++ src/test/java/com/atillaWeb/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atillaWeb.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
