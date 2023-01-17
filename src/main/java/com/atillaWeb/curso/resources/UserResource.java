f9be6cf61fa6c5ed556ea780c319a899843b602e
Atila jr <atilla.junior33@gmail.com>
2023-01-17 18:42:05 -0300

    User entity and resource

---
+++ src/main/java/com/atillaWeb/curso/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.atillaWeb.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atillaWeb.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Atila", "atilla@gmail.com", "994560541", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
