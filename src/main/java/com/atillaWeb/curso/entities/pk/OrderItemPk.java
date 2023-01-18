6ca5400b7a6c87868a28bcd098e1789ce79cabcb
Atila jr <atilla.junior33@gmail.com>
2023-01-18 17:52:09 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/atillaWeb/curso/entities/pk/OrderItemPk.java
@@ -0,0 +1,63 @@
+package com.atillaweb.curso.entities.pk;
+
+import com.atillaweb.curso.entities.Order;
+import com.atillaweb.curso.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @ManyToOne
+  @JoinColumn(name = "order_id")
+  private Order order;
+
+  @ManyToOne
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  public Order getOrder() {
+    return order;
+  }
+
+  public void setOrder(Order order) {
+    this.order = order;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((order == null) ? 0 : order.hashCode());
+    result = prime * result + ((product == null) ? 0 : product.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    OrderItemPk other = (OrderItemPk) obj;
+    if (order == null) {
+      if (other.order != null) return false;
+    } else if (!order.equals(other.order)) return false;
+    if (product == null) {
+      if (other.product != null) return false;
+    } else if (!product.equals(other.product)) return false;
+    return true;
+  }
+}
