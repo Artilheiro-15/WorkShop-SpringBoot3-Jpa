bdd4d35d79da7134e0e3ec519fd7bd1e67c34074
Atila jr <atilla.junior33@gmail.com>
2023-01-18 11:45:54 -0300

    Order Instant ISO 8601  Parte 1

--- src/main/java/com/atillaWeb/curso/entities/User.java
+++ src/main/java/com/atillaWeb/curso/entities/User.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -21,6 +24,13 @@ public class User implements Serializable {
   private String email;
   private String phone;
   private String password;
+
+  @OneToMany(mappedBy = "client")
+  private List<Order> orders = new ArrayList<>();
+
+  public List<Order> getOrders() {
+    return orders;
+  }
 
   public User() {}
 
