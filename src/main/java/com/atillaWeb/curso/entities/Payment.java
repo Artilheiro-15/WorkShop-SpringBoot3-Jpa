94aa8c53af9d12ed164ea710d595f52c83a3ec19
Atila jr <atilla.junior33@gmail.com>
2023-01-19 12:18:57 -0300

    Subtotal & Total methods

--- src/main/java/com/atillaWeb/curso/entities/Payment.java
+++ src/main/java/com/atillaWeb/curso/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.atillaweb.curso.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Payment implements Serializable {
 
   private Instant moment;
 
+  @JsonIgnore
   @OneToOne
   @MapsId
   private Order order;
